9a7185a9f01250cc69f951afd87f31c8b0e5c371
Tim <72915464+LucasCreativeMedia@users.noreply.github.com>
2024-03-12 07:12:57 -0700

    Update index.php

--- ProjectTracker_BS/index.php
+++ ProjectTracker_BS/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Tim Lucas, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.108.0">
     <title>Simple Data</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
