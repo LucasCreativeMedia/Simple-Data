b4e9f370a559670d306c85f0287067cc6435d876
Tim <72915464+LucasCreativeMedia@users.noreply.github.com>
2024-03-12 07:16:33 -0700

    Update footer.php

--- ProjectTracker_BS/includes/footer.php
+++ ProjectTracker_BS/includes/footer.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Tim Lucas, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.108.0">
     <title>Footers · Bootstrap v5.3</title>
 
